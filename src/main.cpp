30d6033fda157707fcd31525b929d5907f21f34e
Benjamin Daniel <postanbenny@gmail.com>
2020-01-23 15:46:44 +0100

    Cleaner code now

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 
-int sensors[2] = { 0 };
-int leftInput = A7;
-int rightInput = A6;
-int leftLight = 6;
-int rightLight = 7;
+#pragma region VARIABLES
+/// Pins
+// Input pins
+const int leftInput = A7;
+const int rightInput = A6;
+// FX pins
+const int leftLight = 6;
+const int rightLight = 7;
+const int piezo = 8;
+// Output/PWM pins
+const int leftMotor = 9;
+const int rightMotor = 10;
+
+/// Datas
+// Predicted colors
+int sensors[2] = { 0 }; // [0] = left sensor, [1] = right sensor
+// Time to be in white for win
 int confidenceThreshold = 2;
-int won = 0;
-bool on = true;
+// Counter to confidenceThreshold
+int winCounter = 0;
+bool victory = false;
+#pragma endregion
 
 /*
 #pragma region ROAST
@@ -112,13 +126,7 @@ int predict(int value, int sensor) {
     return 2;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(leftLight, OUTPUT);
-  pinMode(rightLight, OUTPUT);
-  Serial.begin(9600);
-}
-
+#pragma region MOVEMENT
 void rotateRight(float a) {
   digitalWrite(rightLight, HIGH);
   digitalWrite(leftLight, LOW);
@@ -133,55 +141,55 @@ void moveForward() {
   digitalWrite(leftLight, HIGH);
   digitalWrite(rightLight, HIGH);
 }
+#pragma endregion
 
-bool isBlack(int value) {
-  return value == 0;
-}
-bool isGrey(int value) {
-  return value == 1;
-}
-bool isWhite(int value) {
-  return value == 2;
+#pragma region GET_COLOR
+bool isBlack(int value) { return value == 0; }
+bool isGrey(int value) { return value == 1; }
+bool isWhite(int value) { return value == 2; }
+#pragma endregion
+
+#pragma region VICTORY
+void playVictorySong() {
+  // TODO: Implement song
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(500);
-  if (won > confidenceThreshold) {
-    on = !on;
-        if (on) {
-          moveForward();
-        } else {
-          digitalWrite(leftLight, LOW);
-          digitalWrite(rightLight, LOW);
-        }
-        return;
-  }
-  sensors[0] = predict(analogRead(leftInput), 0);
-  sensors[1] = predict(analogRead(rightInput), 1);
+void doVictoryDance() {
 
+}
+
+void flashVictoryLights() {
+
+}
+#pragma endregion
+
+#pragma region CHECKS
+void checkForBlack() {
   if (isBlack(sensors[1])) {
     if (isBlack(sensors[0])) {
       // B&B - explore / go straight
       moveForward();
-      won = 0;
+      winCounter = 0;
     }
     else if (isGrey(sensors[0])) {
       // B&G - turn counter-clockwise
       rotateLeft(0.05);
-      won = 0;
+      winCounter = 0;
     }
     else if (isWhite(sensors[0])) {
       // B&W - turn clockwise
       rotateRight(0.05);
-      won = 0;
+      winCounter = 0;
     }
   }
+}
+
+void checkForGrey() {
   if (isGrey(sensors[1])) {
     if (isBlack(sensors[0])) {
       // G&B - turn clockwise
       rotateRight(0.02);
-      won = 0;
+      winCounter = 0;
 
     }
     else if (isGrey(sensors[0])) {
@@ -191,26 +199,57 @@ void loop() {
     else if (isWhite(sensors[0])) {
       // G&W - turn counter-clockwise
       rotateLeft(0.05);
-      won++;
+      winCounter++;
     }
   }
+}
+
+void checkForWhite() {
   if (isWhite(sensors[1])) {
     if (isBlack(sensors[0])) {
       // W&B - turn counter-clockwise
       rotateLeft(0.02);
-      won = 0;
+      winCounter = 0;
     }
     else if (isGrey(sensors[0])) {
       // W&G - turn clockwise
       rotateRight(0.04);
-      won++;
+      winCounter++;
     }
     else if (isWhite(sensors[0])) {
       // W&W - go straight / win (stop) if confident enough
-      if (won < confidenceThreshold) {
+      if (winCounter < confidenceThreshold) {
         moveForward();
-        won++;
+        winCounter++;
+      } else {
+        victory = true;
       }
     }
   }
+}
+#pragma endregion
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(leftLight, OUTPUT);
+  pinMode(rightLight, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(500);
+  if (victory) {
+    doVictoryDance();
+    playVictorySong();
+    flashVictoryLights();
+    return;
+  }
+
+  sensors[0] = predict(analogRead(leftInput), 0);
+  sensors[1] = predict(analogRead(rightInput), 1);
+
+  checkForBlack();
+  checkForGrey();
+  checkForWhite();
 }
